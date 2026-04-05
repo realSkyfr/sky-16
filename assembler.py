fae5a8bd828e45788f706422e34b435043874577
Blaise Bennett <blaisebennett@Blaises-MacBook-Air-2.local>
2026-04-05 15:49:29 -0500

    optimizing and building for web runtime

--- assembler.py
+++ assembler.py
@@ -1,198 +1,196 @@
 import struct
 
-tape = b''
-with open('test.s16', 'r') as f:
-    data = f.read()
 
-ins = [line.split() for line in data.strip().splitlines()]
-print(ins)
+def assemble(data: str) -> bytes:
+    tape = b""
 
-def parseregister(reg) -> int:
-    if reg != "x0" and reg.startswith("x"):
-        stripped = reg.removeprefix("x")
-        return int(stripped)
-    elif reg == "p1":
-        return 10
-    elif reg == "p2":
-        return 11
-    elif reg == "car":
-        return 12
-    elif reg == "int":
-        return 13
-    elif reg == "ins":
-        return 14
-    elif reg == "ret":
-        return 15
-    else:
-        raise ValueError(f"Register {reg} does not exist")
+    ins = [line.split() for line in data.strip().splitlines()]
+    print(ins)
 
+    def parseregister(reg) -> int:
+        if reg != "x0" and reg.startswith("x"):
+            stripped = reg.removeprefix("x")
+            return int(stripped)
+        elif reg == "p1":
+            return 10
+        elif reg == "p2":
+            return 11
+        elif reg == "car":
+            return 12
+        elif reg == "int":
+            return 13
+        elif reg == "ins":
+            return 14
+        elif reg == "ret":
+            return 15
+        else:
+            raise ValueError(f"Register {reg} does not exist")
 
-def parseline(ls) -> bytes:
-    match ls[0]:        
-        # Zero Range
-        case 'ADD':
-            lns = [0x00]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+    def parseline(ls) -> bytes:
+        match ls[0]:
+            # Zero Range
+            case "ADD":
+                lns = [0x00]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
 
-        case 'SUB':
-            lns = [0x01]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+            case "SUB":
+                lns = [0x01]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
 
-        case 'MUL':
-            lns = [0x02]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
-  
-        case 'AND':
-            lns = [0x03]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])  
-  
-        case 'OR':
-            lns = [0x04]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])  
-  
-        case 'XOR':
-            lns = [0x05]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])  
-        
-        case 'LFS':
-            lns = [0x06]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
-            
-        case 'RFS':
-            lns = [0x07]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
-            
-        case 'FLP':
-            lns = [0x08]
-            lns.append(parseregister(ls[1]))
-            lns.append(255)
-            return struct.pack(">BBBB", lns[0], lns[1], lns[2])
-            
-        # One Range
-        case 'LOAD':
-            lns = [0x10]
-            lns.append(parseregister(ls[1]))
-            lns.append(int(ls[2], 16))
-            lns.append(255)
-            return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
-            
-        case 'SET':
-            lns = [0x11]
-            lns.append(parseregister(ls[1]))
-            lns.append(int(ls[2], 16))
-            lns.append(255)
-            return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
-            
-        # Two Range
-        case 'VSR':
-            lns = [0x20]
-            lns.append(parseregister(ls[1]))
-            lns.append(255)
-            return struct.pack(">BBB", lns[0], lns[1], lns[2])
-            
-        case 'VSM':
-            lns = [0x20]
-            lns.append(int(ls[1], 16))
-            lns.append(int(ls[2], 16))
-            lns.append(255)
-            return struct.pack(">BHHB", lns[0], lns[1], lns[2], lns[3])
-            
-        case 'VUD':
-            return struct.pack(">BB", 0x23, 255)
-            
-        # Four Range
-        case 'JMP':
-            lns = [0x40]
-            lns.append(int(ls[1], 16))
-            lns.append(255)
-            return struct.pack(">BHB", lns[0], lns[1], lns[2])
-            
-        case 'JIE':
-            lns = [0x41]
-            lns.append(int(ls[1], 16))
-            lns.append(parseregister(ls[2]))
-            lns.append(parseregister(ls[3]))
-            lns.append(255)
-            return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
-            
-        case 'JNE':
-            print(ls)
-            lns = [0x42]
-            lns.append(int(ls[1], 16))
-            lns.append(parseregister(ls[2]))
-            lns.append(parseregister(ls[3]))
-            lns.append(255)
-            return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
-            
-        # Comp Macro
-        case 'JIZ':
-            lns = [0x41]
-            lns.append(int(ls[1], 16))
-            lns.append(parseregister(ls[3]))
-            lns.append(0x00)
-            lns.append(255)
-            return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
-          
-        # Comp Macro  
-        case 'JIC':
-            lns = [0x41]
-            lns.append(0x09)
-            lns.append(0x12)
-            lns.append(255)
-            return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
-        
-        # Five Range
-        case 'MOV':
-            lns = [0x50]
-            lns.append(parseregister(ls[1]))
-            lns.append(int(ls[2], 16))
-            lns.append(255)
-            return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
-        
-        case 'CLN':
-            lns = [0x51]
-            lns.append(parseregister(ls[1]))
-            lns.append(parseregister(ls[2]))
-            lns.append(255)
-            return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
-        
-        # Fth Range
-        case 'HLT':
-            return struct.pack(">BB", 0xFC, 255)
-            
-        case 'NOP':
-            return struct.pack(">BB", 0xFD, 255)
-        
-    raise ValueError(f"Invalid instruction {ls[0]}")
+            case "MUL":
+                lns = [0x02]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
 
-for line in ins:
-    tape += parseline(line)
+            case "AND":
+                lns = [0x03]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
 
-print(tape)
+            case "OR":
+                lns = [0x04]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
 
-with open("out.bin", 'wb') as f:
-    f.write(tape)
+            case "XOR":
+                lns = [0x05]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+
+            case "LFS":
+                lns = [0x06]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+
+            case "RFS":
+                lns = [0x07]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+
+            case "FLP":
+                lns = [0x08]
+                lns.append(parseregister(ls[1]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2])
+
+            # One Range
+            case "LOAD":
+                lns = [0x10]
+                lns.append(parseregister(ls[1]))
+                lns.append(int(ls[2], 16))
+                lns.append(255)
+                return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
+
+            case "SET":
+                lns = [0x11]
+                lns.append(parseregister(ls[1]))
+                lns.append(int(ls[2], 16))
+                lns.append(255)
+                return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
+
+            # Two Range
+            case "VSR":
+                lns = [0x20]
+                lns.append(parseregister(ls[1]))
+                lns.append(255)
+                return struct.pack(">BBB", lns[0], lns[1], lns[2])
+
+            case "VSM":
+                lns = [0x20]
+                lns.append(int(ls[1], 16))
+                lns.append(int(ls[2], 16))
+                lns.append(255)
+                return struct.pack(">BHHB", lns[0], lns[1], lns[2], lns[3])
+
+            case "VUD":
+                return struct.pack(">BB", 0x23, 255)
+
+            # Four Range
+            case "JMP":
+                lns = [0x40]
+                lns.append(int(ls[1], 16))
+                lns.append(255)
+                return struct.pack(">BHB", lns[0], lns[1], lns[2])
+
+            case "JIE":
+                lns = [0x41]
+                lns.append(int(ls[1], 16))
+                lns.append(parseregister(ls[2]))
+                lns.append(parseregister(ls[3]))
+                lns.append(255)
+                return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
+
+            case "JNE":
+                print(ls)
+                lns = [0x42]
+                lns.append(int(ls[1], 16))
+                lns.append(parseregister(ls[2]))
+                lns.append(parseregister(ls[3]))
+                lns.append(255)
+                return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
+
+            # Comp Macro
+            case "JIZ":
+                lns = [0x41]
+                lns.append(int(ls[1], 16))
+                lns.append(parseregister(ls[3]))
+                lns.append(0x00)
+                lns.append(255)
+                return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
+
+            # Comp Macro
+            case "JIC":
+                lns = [0x41]
+                lns.append(0x09)
+                lns.append(0x12)
+                lns.append(255)
+                return struct.pack(">BHBBB", lns[0], lns[1], lns[2], lns[3], lns[4])
+
+            # Five Range
+            case "MOV":
+                lns = [0x50]
+                lns.append(parseregister(ls[1]))
+                lns.append(int(ls[2], 16))
+                lns.append(255)
+                return struct.pack(">BBHB", lns[0], lns[1], lns[2], lns[3])
+
+            case "CLN":
+                lns = [0x51]
+                lns.append(parseregister(ls[1]))
+                lns.append(parseregister(ls[2]))
+                lns.append(255)
+                return struct.pack(">BBBB", lns[0], lns[1], lns[2], lns[3])
+
+            # Fth Range
+            case "HLT":
+                return struct.pack(">BB", 0xFC, 255)
+
+            case "NOP":
+                return struct.pack(">BB", 0xFD, 255)
+
+        raise ValueError(f"Invalid instruction {ls[0]}")
+
+    for line in ins:
+        tape += parseline(line)
+
+    # print(tape)
+
+    return tape
--- emulator.py
+++ emulator.py
@@ -4,218 +4,217 @@
 import array
 import time
 
-# The length is the amount of *BYTES* (not bits or words) between the instruction and the EOL flag
-lenmap = {
-    0x08: 1, # Flip
-    0x10: 3, # Load
-    0x11: 3, # Set
-    
-    0x20: 1, # VSR
-    0x21: 4, # VSM
-    0x22: 0, # VCR
-    0x23: 0, # VUD
-    
-    0x40: 2, # JMP
-    0x41: 4, # JIE
-    0x42: 4, # JNE
-    
-    0x50: 3, # MOV
-    0x51: 2, # CLN
-    
-}
 
-# for non-flip ALU operations
-for i in range(0x0, 0x07):
-    lenmap[i] = 2
-    
-# for F-instructions
-for i in range(0xFC, 0xFF):
-    lenmap[i] = 0
+def emulate(bin: bytes):
+    # The length is the amount of *BYTES* (not bits or words) between the instruction and the EOL flag
+    lenmap = {
+        0x08: 1,  # Flip
+        0x10: 3,  # Load
+        0x11: 3,  # Set
+        0x20: 1,  # VSR
+        0x21: 4,  # VSM
+        0x22: 0,  # VCR
+        0x23: 0,  # VUD
+        0x40: 2,  # JMP
+        0x41: 4,  # JIE
+        0x42: 4,  # JNE
+        0x50: 3,  # MOV
+        0x51: 2,  # CLN
+    }
 
-instructions = []
-registers = array.array('H', [0] * 16)
-registers[9] = 1
-ram = array.array('H', [0] * 32768)
-vram = array.array('B', [0] * 64)
+    # for non-flip ALU operations
+    for i in range(0x0, 0x07):
+        lenmap[i] = 2
 
-with open('out.bin', 'rb') as f:
-    bin = f.read()
+    # for F-instructions
+    for i in range(0xFC, 256):
+        lenmap[i] = 0
 
-while len(bin) > 0:
-    i = int.from_bytes(bin[:1], byteorder='big')
-    length = lenmap[i] + 2
-    instructions.append(bin[:length])
-    bin = bin[length:]
-    
-idx = 0
-endexec = False
-hertz = 1000
-interval = 1 / hertz
-print(f"---BEGIN EXEC {hertz}HZ---")
-while not endexec:
-    start = time.time()
-    match int.from_bytes(instructions[idx][:1], byteorder='big'):
-        # ADD
-        case 0x00:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 + r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) += x{r2i}({r2})")
-            
-        # SUB
-        case 0x01:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 - r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) -= x{r2i}({r2})")
-            
-        # MUL
-        case 0x02:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 * r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) *= x{r2i}({r2})")
-        
-        # AND
-        case 0x03:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 & r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) &= x{r2i}({r2})")
-        
-        # OR
-        case 0x04:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 | r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) |= x{r2i}({r2})")
-        
-        # XOR
-        case 0x05:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 ^ r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) ^= x{r2i}({r2})")
-        
-        # LFS
-        case 0x06:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 << r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) <<= x{r2i}({r2})")
-        
-        # RFS
-        case 0x07:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r1 = registers[r1i]
-            r2 = registers[r2i]
-            registers[11] = r1 # access to p1
-            registers[12] = r2 # access to p2
-            registers[15] = r1 >> r2 # setting ret
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1}) >>= x{r2i}({r2})")
-        
-        # FLP
-        case 0x08:
-            r1i = instructions[idx][1]
-            r1 = registers[r1i]
-            registers[15] = ~r1
-            registers[r1i] = registers[15]
-            print(f"@0x{idx:04x} : x{r1i}({r1})~~")
+    instructions = []
+    registers = array.array("H", [0] * 16)
+    registers[9] = 1
+    ram = array.array("H", [0] * 32768)
+    vram = array.array("B", [0] * 64)
 
-        # JMP
-        case 0x40:
-            addr = int.from_bytes(instructions[idx][1:3], byteorder='big')
-            print(f"@0x{idx:04x} : JMP 0x{addr - 1:04x}")
-            idx = addr - 2
-            
-        # JIE
-        case 0x41:
-            addr = int.from_bytes(instructions[idx][1:3], byteorder='big')
-            r1i = instructions[idx][3]
-            r2i = instructions[idx][4]
-            print(f"@0x{idx:04x} : JIE 0x{addr - 1:04x} (IF {registers[r1i] == registers[r2i]})")
-            if registers[r1i] == registers[r2i]:
-                idx = addr - 2
-            
-        # JNE
-        case 0x42:
-            addr = int.from_bytes(instructions[idx][1:3], byteorder='big')
-            r1i = instructions[idx][3]
-            r2i = instructions[idx][4]
-            print(f"@0x{idx:04x} : JIE 0x{addr - 1:04x} (IF {registers[r1i] != registers[r2i]})")
-            if registers[r1i] != registers[r2i]:
+    while len(bin) > 0:
+        i = bin[0]
+        length = lenmap[i] + 2
+        instructions.append(bin[:length])
+        bin = bin[length:]
+
+    idx = 0
+    endexec = False
+    hertz = 1000
+    interval = 1 / hertz
+    print(f"---BEGIN EXEC {hertz}HZ---")
+    while not endexec:
+        start = time.time()
+        match int.from_bytes(instructions[idx][:1], byteorder="big"):
+            # ADD
+            case 0x00:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = (r1 + r2) & 0xFFFF  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) += x{r2i}({r2})")
+
+            # SUB
+            case 0x01:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 - r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) -= x{r2i}({r2})")
+
+            # MUL
+            case 0x02:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 * r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) *= x{r2i}({r2})")
+
+            # AND
+            case 0x03:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 & r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) &= x{r2i}({r2})")
+
+            # OR
+            case 0x04:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 | r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) |= x{r2i}({r2})")
+
+            # XOR
+            case 0x05:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 ^ r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) ^= x{r2i}({r2})")
+
+            # LFS
+            case 0x06:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 << r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) <<= x{r2i}({r2})")
+
+            # RFS
+            case 0x07:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r1 = registers[r1i]
+                r2 = registers[r2i]
+                registers[11] = r1  # access to p1
+                registers[12] = r2  # access to p2
+                registers[15] = r1 >> r2  # setting ret
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1}) >>= x{r2i}({r2})")
+
+            # FLP
+            case 0x08:
+                r1i = instructions[idx][1]
+                r1 = registers[r1i]
+                registers[15] = ~r1
+                registers[r1i] = registers[15]
+                print(f"@0x{idx:04x} : x{r1i}({r1})~~")
+
+            # JMP
+            case 0x40:
+                addr = int.from_bytes(instructions[idx][1:3], byteorder="big")
+                print(f"@0x{idx:04x} : JMP 0x{addr - 1:04x}")
                 idx = addr - 2
 
-        # MOV
-        case 0x50:
-            register = instructions[idx][1]
-            val = int.from_bytes(instructions[idx][2:4], byteorder='big')
-            registers[register] = val
-            print(f"@0x{idx:04x} : x{register} = {val}")
-        
-        # CLN
-        case 0x51:
-            r1i = instructions[idx][1]
-            r2i = instructions[idx][2]
-            r2 = registers[r2i]
-            registers[r1i] = r2
-            print(f"@0x{idx:04x} : x{r1i} = {r2}")
-        
-        # HLT
-        case 0xFC:
-            print(f"@0x{idx:04x} : HALT")
-            endexec = True
-            
-        # NOP
-        case 0xFD:
-            print(f"@0x{idx:04x} : NOP")
-        
-        case _:
-            print(f"@0x{idx:04x} : EXCEPTION, INVALID OPERATION")
-            endexec = True
-            
-    print("@REGIST", registers.tolist())
-        
-    elapsed = time.time() - start
-    if elapsed < interval:
-        time.sleep(interval - elapsed)
-    
-    idx += 1
-print("---END EXEC---")
+            # JIE
+            case 0x41:
+                addr = int.from_bytes(instructions[idx][1:3], byteorder="big")
+                r1i = instructions[idx][3]
+                r2i = instructions[idx][4]
+                print(
+                    f"@0x{idx:04x} : JIE 0x{addr - 1:04x} (IF {registers[r1i] == registers[r2i]})"
+                )
+                if registers[r1i] == registers[r2i]:
+                    idx = addr - 2
+
+            # JNE
+            case 0x42:
+                addr = int.from_bytes(instructions[idx][1:3], byteorder="big")
+                r1i = instructions[idx][3]
+                r2i = instructions[idx][4]
+                print(
+                    f"@0x{idx:04x} : JNE 0x{addr - 1:04x} (IF {registers[r1i] != registers[r2i]})"
+                )
+                if registers[r1i] != registers[r2i]:
+                    idx = addr - 2
+
+            # MOV
+            case 0x50:
+                register = instructions[idx][1]
+                val = int.from_bytes(instructions[idx][2:4], byteorder="big")
+                registers[register] = val
+                print(f"@0x{idx:04x} : x{register} = {val}")
+
+            # CLN
+            case 0x51:
+                r1i = instructions[idx][1]
+                r2i = instructions[idx][2]
+                r2 = registers[r2i]
+                registers[r1i] = r2
+                print(f"@0x{idx:04x} : x{r1i} = {r2}")
+
+            # HLT
+            case 0xFC:
+                print(f"@0x{idx:04x} : HALT")
+                endexec = True
+
+            # NOP
+            case 0xFD:
+                print(f"@0x{idx:04x} : NOP")
+
+            case _:
+                print(f"@0x{idx:04x} : EXCEPTION, INVALID OPERATION")
+                endexec = True
+
+        print("@REGIST", registers.tolist())
+
+        elapsed = time.time() - start
+        if elapsed < interval:
+            time.sleep(interval - elapsed)
+
+        idx += 1
+    print("---END EXEC---")
